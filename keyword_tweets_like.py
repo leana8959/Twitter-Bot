befab58e8de0531a7e8be2f0aefabe8ae626b849
Léana 江 <leana.jiang@icloud.com>
2022-12-26 15:29:26 +0100

    feat(keyword_tweets_like): added parser

--- keyword_tweets_like.py
+++ keyword_tweets_like.py
@@ -1,18 +1,24 @@
 import os
 from twitter_bot_class import Twitter_Bot
+import argparse
+
 """
 Likes tweets based on the keywords you entered
 """
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Like tweets associated to a keyword")
+    parser.add_argument("--username", type=str, help="Username of your account")
+    parser.add_argument("--password", type=str, help="Password of your account")
+    parser.add_argument("--keyword", type=str, help="Keyword of your choice")
+    args = parser.parse_args().__dict__
+    USERNAME = args['username']
+    PASSWORD = args['password']
+    KEYWORD = args['keyword']
+
     try:
-        EMAIL = input("Provide your Phone, email or username: ")
-        PASSWORD = input("Provide your password: ")
-        tbot = Twitter_Bot(EMAIL, PASSWORD)
+        tbot = Twitter_Bot(USERNAME, PASSWORD)
         tbot.login()
-        tbot.search('100DaysOfCode')
-        tbot.like_tweets(10)
-        tbot.search('hacktober')
+        tbot.search(KEYWORD)
         tbot.like_tweets(10)
         tbot.logout()
     except Exception as e:
